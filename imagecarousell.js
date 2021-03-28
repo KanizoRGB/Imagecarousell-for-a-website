0e6fae828651cea28ef97a85444f9b2e41d222d8
KanizoRGB <65230762+KanizoRGB@users.noreply.github.com>
2021-03-28 15:55:11 +0300

    Update imagecarousell.js

--- imagecarousell.js
+++ imagecarousell.js
@@ -26,3 +26,17 @@ function SlideShow(n) {
   slides[slidePosition-1].style.display = "block";
   circles[slidePosition-1].className += " enable";
 } 
+var slidePosition = 0;
+SlideShow();
+
+function SlideShow() {
+  var i;
+  var slides = document.getElementsByClassName("Containers");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slidePosition++;
+  if (slidePosition > slides.length) {slidePosition = 1}
+  slides[slidePosition-1].style.display = "block";
+  setTimeout(SlideShow, 2000); // Change image every 2 seconds
+} 
