840102f0e5625968f71edf2554a034b6cad4071a
KanizoRGB <65230762+KanizoRGB@users.noreply.github.com>
2021-03-28 15:54:12 +0300

    Update imagecarousell.js

--- imagecarousell.js
+++ imagecarousell.js
@@ -1,50 +1,28 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var slidePosition = 1;
+SlideShow(slidePosition);
 
-// Next/previous controls
+// forward/Back controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  SlideShow(slidePosition += n);
 }
 
-// Thumbnail image controls
+//  images controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  SlideShow(slidePosition = n);
 }
 
-function showSlides(n) {
+function SlideShow(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  var slides = document.getElementsByClassName("Containers");
+  var circles = document.getElementsByClassName("dots");
+  if (n > slides.length) {slidePosition = 1}
+  if (n < 1) {slidePosition = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+  for (i = 0; i < circles.length; i++) {
+      circles[i].className = circles[i].className.replace(" enable", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
-```
-**N/B**
-
-Remember to link the .css file and the .js file to your HTMl file. To remind yourself click [ here ](https://www.w3schools.com/tags/att_script_src.asp) for css or  click [ here ](https://www.w3schools.com/tags/att_script_src.asp)js.
-
-### Automatic slide-show
-In order to make the images in the carousel change automatically without the user input,we need to modify our Javascript code. Use the following code. In this code,we include a timer of 2 seconds.
-```js
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  slides[slidePosition-1].style.display = "block";
+  circles[slidePosition-1].className += " enable";
 } 
